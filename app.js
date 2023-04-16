3d22dfed4b61d7022718d771619efb1e0c62c039
Joel Sellgren <joel.sellgren@chasacademy.se>
2023-04-16 10:03:40 +0200

    added create and read

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser")
 const express = require("express")
 const exphbs = require("express-handlebars")
 const { MongoClient } = require("mongodb")
@@ -18,3 +19,37 @@ app.set("view engine", "hbs");
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
+
+const getWorkshopCollection = async () => {
+    await client.connect()
+    const db = client.db(dbName)
+    const collection = db.collection("workshop")
+
+    return collection
+}
+
+app.get("/", async (req, res) => {
+    const collection = await getWorkshopCollection()
+    const findResult = await collection.find({}).toArray()
+    console.log(findResult)
+    res.render("home", {workshop: findResult})
+})
+
+app.get("/new-note", async (req, res) => {
+    res.render("new-note")
+})
+
+app.post("/new-note", async (req, res) => {
+    const newNote = {
+        desc: req.body.desc,
+    }
+
+    const collection = await getWorkshopCollection()
+    await collection.insertOne(newNote)
+
+    res.redirect("/")
+})
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000")
+})
